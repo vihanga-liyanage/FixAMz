a680a2475cce07f531469947690db9c0fd2d142c
tole454 <dineth454@gmail.com>
2015-11-16 10:47:00 +0530

    add cosID datafeild to add user function 50%

--- AdminUserPeopleTab.aspx.designer.cs
+++ AdminUserPeopleTab.aspx.designer.cs
@@ -175,6 +175,24 @@ namespace FixAMz_WebApplication {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl AddNewContactValidator;
         
         /// <summary>
+        /// AddNewCostTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox AddNewCostTextBox;
+        
+        /// <summary>
+        /// AddNewCostValidator control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl AddNewCostValidator;
+        
+        /// <summary>
         /// TypeDropDown control.
         /// </summary>
         /// <remarks>
