87d7e4edbfd0ecee9633e121444fa63813db5856
vihanga <vihangaliyanage007@gmail.com>
2015-11-17 15:20:26 +0530

    NotificationView page header changed

--- NotificationView.aspx.designer.cs
+++ NotificationView.aspx.designer.cs
@@ -31,6 +31,24 @@ namespace FixAMz_WebApplication {
         protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSourceFixAMz;
         
         /// <summary>
+        /// userName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl userName;
+        
+        /// <summary>
+        /// A1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor A1;
+        
+        /// <summary>
         /// responseBoxGreen control.
         /// </summary>
         /// <remarks>
